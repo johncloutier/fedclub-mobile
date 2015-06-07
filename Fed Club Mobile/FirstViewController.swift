211d4a068427bdfb8284c825bf47de4039f34068
John Cloutier <cloutier.john@gmail.com>
2015-06-07 08:43:12 -0400

    Initial Commit

---
+++ Fed Club Mobile/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Fed Club Mobile
+//
+//  Created by John Cloutier on 6/7/15.
+//  Copyright (c) 2015 John Cloutier. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Fed Club MobileTests/Fed_Club_MobileTests.swift
@@ -0,0 +1,36 @@
+//
+//  Fed_Club_MobileTests.swift
+//  Fed Club MobileTests
+//
+//  Created by John Cloutier on 6/7/15.
+//  Copyright (c) 2015 John Cloutier. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Fed_Club_MobileTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
